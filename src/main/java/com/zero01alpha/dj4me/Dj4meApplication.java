c28a66150403afe15f0f1619356d2c52bdd5522f
mikelhamer <mikelhamer@gmail.com>
2017-01-26 22:20:16 -0500

    Initial commit

---
+++ src/main/java/com/zero01alpha/dj4me/Dj4meApplication.java
@@ -0,0 +1,12 @@
+package com.zero01alpha.dj4me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dj4meApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dj4meApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zero01alpha/dj4me/Dj4meApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zero01alpha.dj4me;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Dj4meApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
